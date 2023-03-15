fe9cbe1f438f0c1e799a4e046a27a0ff6833e658
amirfazel <amirfazel45@gmail.com>
2023-03-15 11:27:43 +0330

    altering time in setting modal initialized

--- src/components/Modals/SettingModal/index.tsx
+++ src/components/Modals/SettingModal/index.tsx
@@ -1,29 +1,61 @@
-import { FC, MouseEventHandler, PropsWithChildren } from "react";
+import { FC, MouseEventHandler, PropsWithChildren, useContext } from "react";
 import { AiFillSetting, AiOutlineClose } from "react-icons/ai";
+import { TfiTimer} from "react-icons/tfi";
 import HeaderItem from "../../Layout/Header/HeaderItem";
+import { AppContext } from "../../../context/store";
 
-interface SettingModalProps extends PropsWithChildren{
-    onClose:MouseEventHandler<HTMLDivElement>
+interface SettingModalProps extends PropsWithChildren {
+  onClose: MouseEventHandler<HTMLDivElement>;
 }
 
+const SettingModal: FC<SettingModalProps> = ({ onClose }) => {
 
-const SettingModal:FC<SettingModalProps> = ({onClose}) => {
-    return(
+    const state = useContext(AppContext).state;
+
+
+    
+
+  return (
     <>
-    <div className='fixed top-0 left-0 w-full h-[100vh] z-10 bg-[#000000bf]' onClick={onClose}/>
-        <div className='fixed top-[14vh] left-[20%] w-[60%] z-40 overflow-y-auto bg-yellow-200 rounded-t-md flex flex-col'>
+      <div
+        className="fixed top-0 left-0 w-full h-[100vh] z-10 bg-[#000000bf]"
+        onClick={onClose}
+      />
+      <div className="fixed top-[14vh] left-[20%] w-[60%] z-40 overflow-y-auto bg-yellow-200 rounded-t-md flex flex-col gap-5 p-2">
         <div className="flex justify-between items-center px-4">
-        <HeaderItem>
-              <AiFillSetting />
-              <h3 className="font-bold text-md hidden md:inline-block">Setting</h3>
-        </HeaderItem>
-        <span className="cursor-pointer" onClick={onClose}>
-            <AiOutlineClose />  
-        </span>
+          <HeaderItem>
+            <AiFillSetting />
+            <h3 className="font-bold text-md hidden md:inline-block">
+              Setting
+            </h3>
+          </HeaderItem>
+          <span className="cursor-pointer" onClick={onClose}>
+            <AiOutlineClose />
+          </span>
         </div>
-        <p>aha</p>
+        <div className="flex flex-col p-4">
+            <span className="flex font-bold text-slate-600 gap-3 justify-start items-center">
+                <TfiTimer />
+                <h3>Timer (minutes)</h3>
+            </span>
+            <span className="flex items-center justify-evenly">
+                    <div className="flex flex-col">
+                        <h3>Pomodoro</h3>
+                        <input type={"number"} value={state.timer.seconds}></input>
+                    </div>
+                    <div className="flex flex-col">
+                        <h3>Short Break</h3>
+                        <input type={"number"} value={state.timer.seconds}></input>
+                    </div>
+                    <div className="flex flex-col">
+                        <h3>Long Break</h3>
+                        <input type={"number"} value={state.timer.seconds}></input>
+                    </div>
+            </span>
         </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
-export default SettingModal
+export default SettingModal;
