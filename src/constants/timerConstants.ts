e40206458138cd52d7dd178a13606ddc9eb43ba1
amirfazel <amirfazel45@gmail.com>
2023-03-15 12:40:35 +0330

    constants value updated

--- src/constants/timerConstants.ts
+++ src/constants/timerConstants.ts
@@ -2,8 +2,8 @@ import {TimerActionTypes, TimerState} from '../@types/context/context.types';
 export const timerStates: TimerState[] = [
     {
         timerMode: TimerActionTypes.FOCUS_MODE,
-        minutes:0,
-        seconds:6,
+        minutes:60,
+        seconds:0,
         backGroundColor:"#fc7f03",
         id:"1",
     },
@@ -16,8 +16,8 @@ export const timerStates: TimerState[] = [
     },
     {
         timerMode:TimerActionTypes.LONG_BREAK_MODE,
-        minutes:0,
-        seconds:10,
+        minutes:20,
+        seconds:0,
         backGroundColor:"#d25ff5",
         id:"13",
     },
